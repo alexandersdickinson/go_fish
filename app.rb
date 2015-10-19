cc1d3d8e394fc80d7bf68f31a4b6f38ca96db922
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-10-18 22:34:08 -0700

    Spec written for a successful card retrieval.

--- app.rb
+++ app.rb
@@ -31,10 +31,29 @@ post('/new/:id') do
   erb(:game)
 end
 
-post('/:id/form') do
+get('/:id/form') do
   @game = GoFishGame.find(params.fetch('id').to_i())
   @current_player = @game.current_player()
   @active_players = @game.active_players()
   @header = "#{@game.current_player().name()}\'s Turn"
   erb(:game)
+end
+
+post('/:id/result') do
+  @header = "Result"
+  @game = GoFishGame.find(params.fetch('id').to_i())
+  last_current_player = @game.current_player()
+  target_card = Card.new({:suit => "Spades", :value => params.fetch('player-card')})
+  target_player = @game.players()[params.fetch('target-player').to_i()]
+  target_player_last_hand = target_player.hand().cards().length()
+  @game.turn(target_player, target_card)
+  target_player_current_hand = target_player.hand().cards().length()
+  current_player = @game.current_player()
+  if current_player == last_current_player
+    @message = "You retrieved #{target_player_last_hand - target_player_current_hand} cards with the value #{target_card.value()} from #{target_player.name()}. Click continue to resume your turn."
+  else
+    @message = "No card with the value #{target_card.value().capitalize()} was found. It is now #{current_player.name()}\'s turn."
+  end
+  @card_found
+  erb(:result)
 end
--- spec/go_fish_integration_spec.rb
+++ spec/go_fish_integration_spec.rb
@@ -16,7 +16,7 @@ describe('the game creation path', :type => :feature) do
     fill_in('player-name1', :with => 'Jim')
     click_button('Start Game')
     click_link("Back")
-    expect(page).to(have_content("Game 1"))
+    expect(page).to(have_content("Ralph and Jim's Game"))
   end
   
   it('allows the user to name every player in the game') do
@@ -76,8 +76,109 @@ describe('the turn path', :type => :feature) do
     fill_in('player-name0', :with => 'Ralph')
     fill_in('player-name1', :with => 'Jim')
     click_button('Start Game')
+    expect(page).to(have_content("Your books: 0"))
+    expect(page).to(have_content("Books: 0"))
   end
   
-  it('displays appropriate information on the other players') do
+  it('informs the player when they card they were looking for is not found') do
+    visit('/')
+    fill_in('player_count', :with => '2')
+    click_button("New Game")
+    fill_in('player-name0', :with => 'Ralph')
+    fill_in('player-name1', :with => 'Jim')
+    click_button('Start Game')
+    bad_card = nil
+    game = GoFishGame.all()[0]
+    player1_values = []
+    player2_values = []
+    values = []
+    player1 = game.players()[0]
+    player2 = game.players()[1]
+    match_card = nil
+    loop do
+      player1_values = []
+      player1.hand().cards().each() do |card|
+        player1_values.push(card.value())
+      end
+      player1_values.uniq!()
+      player2_values = []
+      player2.hand().cards().each() do |card|
+        player2_values.push(card.value())
+      end
+      player2_values.uniq!()
+      values = player1_values - player2_values
+      break if values != []
+      matches = player1_values + player2_values
+      matches.uniq!
+      match_card = nil
+      player1.hand().cards().each() do |card|
+        if matches[0] == card.value()
+          match_card = card
+          break
+        end
+      end
+      choose("#{match_card.suit().downcase()}-#{match_card.value().downcase()}-radio")
+      choose("#{player2.name()}")
+      click_button("Ask For Card")
+      click_button("Continue")
+    end
+    player1.hand().cards().each() do |card|
+      if values[0] == card.value()
+        match_card = card
+        break
+      end
+    end
+    choose("#{match_card.suit().downcase()}-#{match_card.value().downcase()}-radio")
+    choose("#{player2.name()}")
+    click_button("Ask For Card")
+    expect(page).to(have_content("No card with the value #{values[0]} was found. It is now Jim\'s turn."))
+  end
+  
+  it('informs the player that the desired card has been found') do
+    visit('/')
+    fill_in('player_count', :with => '2')
+    click_button("New Game")
+    fill_in('player-name0', :with => 'Ralph')
+    fill_in('player-name1', :with => 'Jim')
+    click_button('Start Game')
+    good_card = nil
+    game = GoFishGame.all()[0]
+    player1_values = []
+    player2_values = []
+    values = []
+    player1 = nil
+    player2 = nil
+    loop do
+      player1 = game.current_player()
+      player2 = (game.players() - [player1])[0]
+      player1_values = []
+      player1.hand().cards().each() do |card|
+        player1_values.push(card.value())
+      end
+      player2.hand().cards().each() do |card|
+        player2_values.push(card.value())
+      end
+      values = player1_values & player2_values
+      break if values != []
+      choose("#{player1.hand.cards()[0].suit()}-#{player1.hand.cards()[0].value()}-radio")
+      choose("#{player2.name()}")
+      click_button("Ask For Card")
+      click_button("Continue")
+    end
+    match_card = nil
+    player1.hand().cards().each() do |card|
+      if values[0] == card.value()
+        match_card = card
+        break
+      end
+    end
+    card_count = 0
+    player2.hand.cards().each() do |card|
+      card_count = card_count + 1 if values[0] == card.value()
+    end
+    choose("#{match_card.suit().downcase()}-#{match_card.value().downcase()}-radio")
+    choose("#{player2.name()}")
+    click_button("Ask For Card")
+    expect(page).to(have_content("You retrieved #{card_count} cards with the value #{values[0]} from #{player2.name()}. Click continue to resume your turn."))
   end
 end
